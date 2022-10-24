b48d44e54d0d7176b78630e35648be154ac32f74
ashanti <ammanier2@gmail.com>
2022-10-23 22:12:43 -0400

    find one book error handling

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Book:
     def __init__(self, id, title, description):
@@ -30,14 +30,46 @@ def handle_books():
     return jsonify(books_response)
 
 
-#Find one book
-@books_bp.route("/<book_id>", methods=["GET"]) #postman request => url + /books/3 where 3 == book_id
-def handle_book(book_id):
-    book_id = int(book_id)
+# #Find One Book without Refactoring
+# @books_bp.route("/<book_id>", methods=["GET"]) #postman request => url + /books/3 where 3 == book_id
+# def handle_book(book_id):
+#     # Handling an invalid book_id
+#     try:
+#         book_id = int(book_id)
+#     except:
+#         return {"message":f"book {book_id} invalid"}, 400
+
+#     for book in books:
+#         if book.id == book_id:
+#             return {
+#                 "id": book.id,
+#                 "title": book.title,
+#                 "description": book.description,
+#             }
+#     # Handling a non-existing book
+#     return {"message":f"book {book_id} not found"}, 404 
+
+#---------------- Refactoring Finding One Book w/Helper Function ----------------------------------
+#Helper Function using error handling logic 
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+
     for book in books:
         if book.id == book_id:
-            return {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description,
-            }
+            return book
+
+    abort(make_response({"message":f"book {book_id} not found"}, 404))
+
+#Updated Find One Book with helper function
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book = validate_book(book_id) #helper function
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description,
+    }
