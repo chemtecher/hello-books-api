5c3f75e1533be1c02a3e22570894525c881f8a0f
ashanti <ammanier2@gmail.com>
2022-10-23 21:24:34 -0400

    find one book

--- app/routes.py
+++ app/routes.py
@@ -15,6 +15,7 @@ books = [
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
+#Read All books
 @books_bp.route("", methods=["GET"])
 def handle_books():
     books_response = []
@@ -27,3 +28,16 @@ def handle_books():
             }
         )
     return jsonify(books_response)
+
+
+#Find one book
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book_id = int(book_id)
+    for book in books:
+        if book.id == book_id:
+            return {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description,
+            }
