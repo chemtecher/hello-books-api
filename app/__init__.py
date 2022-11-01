e4c06f8b528f02331b03deb62bb7012db0a4d0c9
ashanti <ammanier2@gmail.com>
2022-11-01 00:45:22 -0400

    added refactored code to read one book

--- app/__init__.py
+++ app/__init__.py
@@ -11,13 +11,14 @@ def create_app(test_config=None):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
 
+    # Import models here
     db.init_app(app)
     migrate.init_app(app, db)
 
     from app.models.book import Book
 
     # Register Blueprints here
-    # from .routes import books_bp
-    # app.register_blueprint(books_bp)
+    from .routes import books_bp
+    app.register_blueprint(books_bp)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,54 +1,81 @@
+from os import abort
+from app import db
+from app.models.book import Book
+from flask import Blueprint, jsonify, make_response, request
 
-from flask import Blueprint, jsonify, abort, make_response
-
-# books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
-
-# class Book:
-#     def __init__(self, id, title, description):
-#         self.id = id
-#         self.title = title
-#         self.description = description
-
-# books = [
-#     Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
-#     Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
-#     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
-# ]
+books_bp = Blueprint("books", __name__, url_prefix="/books")
 
 
-# #Read All books
-# @books_bp.route("", methods=["GET"])
-# def handle_books():
-#     books_response = []
+#Get a single Book
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+
+    book = Book.query.get(book_id)
+
+    if not book:
+        abort(make_response({"message":f"book {book_id} not found"}, 404))
+
+    return book
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def read_one_book(book_id):
+    book = validate_book(book_id)
+    return {
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        }
+
+
+@books_bp.route("", methods=["POST"])
+def create_book():
+    request_body = request.get_json()
+    new_book = Book(title=request_body["title"],
+                    description=request_body["description"])
+
+    db.session.add(new_book)
+    db.session.commit()
+
+    return make_response(f"Book {new_book.title} successfully created", 201)
+
+
+#Read All books
+@books_bp.route("", methods=["GET"])
+def read_all_books():
+    books_response = []
+    books = Book.query.all()
+    for book in books:
+        books_response.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        )
+    return jsonify(books_response)
+
+
+# #Find One Book without Refactoring
+# @books_bp.route("/<book_id>", methods=["GET"]) #postman request => url + /books/3 where 3 == book_id
+# def handle_book(book_id):
+#     # Handling an invalid book_id
+#     try:
+#         book_id = int(book_id)
+#     except:
+#         return {"message":f"book {book_id} invalid"}, 400
+
 #     for book in books:
-#         books_response.append(
-#             {
+#         if book.id == book_id:
+#             return {
 #                 "id": book.id,
 #                 "title": book.title,
-#                 "description": book.description
+#                 "description": book.description,
 #             }
-#         )
-#     return jsonify(books_response)
-
-
-# # #Find One Book without Refactoring
-# # @books_bp.route("/<book_id>", methods=["GET"]) #postman request => url + /books/3 where 3 == book_id
-# # def handle_book(book_id):
-# #     # Handling an invalid book_id
-# #     try:
-# #         book_id = int(book_id)
-# #     except:
-# #         return {"message":f"book {book_id} invalid"}, 400
-
-# #     for book in books:
-# #         if book.id == book_id:
-# #             return {
-# #                 "id": book.id,
-# #                 "title": book.title,
-# #                 "description": book.description,
-# #             }
-# #     # Handling a non-existing book
-# #     return {"message":f"book {book_id} not found"}, 404 
+#     # Handling a non-existing book
+#     return {"message":f"book {book_id} not found"}, 404 
 
 # #---------------- Refactoring Finding One Book w/Helper Function ----------------------------------
 # #Helper Function using error handling logic 
@@ -74,3 +101,26 @@ from flask import Blueprint, jsonify, abort, make_response
 #         "title": book.title,
 #         "description": book.description,
 #     }
+
+# @books_bp.route("", methods=["POST"])
+# def handle_books():
+#     request_body = request.get_json()
+#     new_book = Book(title=request_body["title"],
+#                     description=request_body["description"])
+
+#     db.session.add(new_book)
+#     db.session.commit()
+
+#     return make_response(f"Book {new_book.title} successfully created", 201)
+
+# class Book:
+#     def __init__(self, id, title, description):
+#         self.id = id
+#         self.title = title
+#         self.description = description
+
+# books = [
+#     Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
+#     Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
+#     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+# ]
